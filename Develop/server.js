e1d2a598f3604b1b21ce33c976b170c13045c523
Rob Kellen <rob.h.kellen@gmail.com>
2020-09-23 22:26:13 -0700

    deleted user.js from models

--- Develop/server.js
+++ Develop/server.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 //connect to our server
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/populatedb", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
