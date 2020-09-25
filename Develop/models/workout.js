40515d20d29918a0fd1aba0bad40d89be583aea0
Rob Kellen <rob.h.kellen@gmail.com>
2020-09-24 17:50:21 -0700

    created workout schema

--- Develop/models/workout.js
+++ Develop/models/workout.js
@@ -5,6 +5,49 @@ const Schema = mongoose.Schema;
 const WorkoutSchema = new Schema({
   day: {
     type: Date,
-    default: Date.now
-  }
-})
+    default: Date.now,
+  },
+  exercise: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: true,
+      },
+      weight: {
+        type: Number,
+        default: 0,
+        required: true,
+      },
+      sets: {
+        type: Number,
+        default: 0,
+        required: true,
+      },
+      reps: {
+        type: Number,
+        default: 0,
+        required: true,
+      },
+      duration: {
+        type: Number,
+        default: 0,
+        required: true,
+      },
+      distance: {
+        type: Number,
+        default: 0,
+        required: true,
+      }
+    },
+  ],
+});
+
+const Workout = new Schema("Workout", WorkoutSchema);
+
+module.exports = Workout
